01a9faa10b4b665e22cd7b51df45f89100350de9
Dahiver Guerra <roddavid1@gmail.com>
2025-06-11 01:15:24 -0400

    Redesigned the Hero

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,10 +1,18 @@
-export default function HeroSection() {
+import { useNavigate } from "react-router-dom";
+
+export default function HeroSection({ onRecentClick }) {
+
+  const navigate = useNavigate();
   return (
     <section className="hero">
-      <h2>Welcome to the Campus Lost and Found!</h2>
-      <p>
-        Maecenas congue vestibulum nisl sagittis hendrerit. Phasellus sed mattis ex. Pellentesque vulputate leo at justo venenatis consequat. Morbi ac ipsum augue. Praesent vitae maximus augue. Etiam convallis augue sit amet purus posuere, tincidunt imperdiet diam vehicula. Pellentesque non pharetra ligula. Quisque nec vulputate ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam non neque luctus justo laoreet congue nec ac turpis. Vestibulum laoreet egestas metus id varius. Aliquam eros turpis, tempor quis lorem auctor, euismod gravida est. Mauris fringilla elit ut velit mattis dapibus.
-      </p>
+      <div className="hero-darken"></div> {/* darken overlay background */}
+      <div className="hero-overlay">
+        <h2>Your campus item recovery hub</h2>
+        <div className="hero-buttons">
+          <button className="base-button lost-button" onClick={() => navigate('/report')}>Report Lost Item</button>
+          <button className="base-button recent-post" onClick={onRecentClick}>View Recent Posts</button>
+        </div>
+      </div>
     </section>
   );
 }
