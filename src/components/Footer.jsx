9ff7591d7582bba4884e967434b5d2b7b2c01c98
Dahiver Guerra <roddavid1@gmail.com>
2025-06-11 01:10:50 -0400

    Created Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return(
+    <footer className="footer">
+      <p>Set up your own Campus Lost and Found!</p>
+      <a href="https://github.com/Dreamzur/Campus-Lost-and-Found" target="_blank" rel="noopener noreferrer">GitHub</a>
+    </footer>
+  );
+}
