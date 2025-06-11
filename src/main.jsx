ca68910a2d9b3ae8680480ea65eefd0e37fff0ba
Dahiver Guerra <roddavid1@gmail.com>
2025-06-11 01:16:35 -0400

    Added paths to main

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App.jsx';
+import ReportLost from './Pages/ReportLost.jsx';
+import Layout from './components/Layout.jsx';
+import { itemSubmitHandler } from './utils/api.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<App />} />
+          <Route path="report" element={<ReportLost onSubmitPost={itemSubmitHandler} />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
