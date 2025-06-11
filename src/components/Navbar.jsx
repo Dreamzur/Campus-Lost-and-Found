3f38ff74d2c843d8640b822c5da1ea7f86b69f13
Dahiver Guerra <roddavid1@gmail.com>
2025-06-11 01:15:53 -0400

    Made changes to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,8 @@
-export default function Navbar({ onPostClick }) {
+export default function Navbar() {
   return (
     <header className="navbar">
       <h1>Campus Lost and Found</h1>
       <nav>
-        <a href="#">Home</a>
-        <a href="#" onClick={onPostClick}>Post an Item</a>
         <a href="#">Sign In</a>
       </nav>
     </header>
