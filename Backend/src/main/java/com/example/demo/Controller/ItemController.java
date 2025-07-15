8adf56b3f9057de28bdb3538be9dfcb7c98af5a9
Dahiver Guerra <roddavid1@gmail.com>
2025-07-14 23:08:46 -0400

    added Postmapping

--- Backend/src/main/java/com/example/demo/Controller/ItemController.java
+++ Backend/src/main/java/com/example/demo/Controller/ItemController.java
@@ -4,17 +4,18 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.example.demo.model.FoundItem;
 import com.example.demo.model.LostItem;
 import com.example.demo.repository.FoundItemRepository;
 import com.example.demo.repository.LostItemRepository;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:5173") // frontend
 public class ItemController {
 
     private final LostItemRepository lostRepo;
@@ -33,6 +34,12 @@ public class ItemController {
     @GetMapping("/found")
     public List<FoundItem> getFoundItems() {
         return toList(foundRepo.findAll());
+    }
+
+    @PostMapping("/lost")
+    public ResponseEntity<LostItem> createLostItem(@RequestBody LostItem item) {
+        LostItem saved = lostRepo.save(item);
+        return new ResponseEntity<>(saved, HttpStatus.CREATED);
     }
 
     private <T> List<T> toList(Iterable<T> iterable) {
