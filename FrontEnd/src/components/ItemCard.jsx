6c3b1b23f2c4b6394f3e517dc56d89be91a64a32
Dahiver Guerra <roddavid1@gmail.com>
2025-07-14 22:54:23 -0400

    Typo fix
    item.desc -> item.description

--- FrontEnd/src/components/ItemCard.jsx
+++ FrontEnd/src/components/ItemCard.jsx
@@ -7,7 +7,7 @@ export default function ItemCard({ item }) {
       </a>
       }
       <h3 className="item-card-title">{item.title}</h3>
-      <p className="item-card-description">{item.desc}</p>
+      <p className="item-card-description">{item.description}</p>
       <div className="item-card-footer"> 
         <p className="item-card-footer-location">{item.location}</p>
         <button className="item-card-footer-contact" onClick={() => {
