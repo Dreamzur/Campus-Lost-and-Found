6768b58975338683ad07270da61bf4feb472dcf8
Dahiver Guerra <roddavid1@gmail.com>
2025-07-14 23:09:31 -0400

    Connected front to backend

--- FrontEnd/src/utils/api.js
+++ FrontEnd/src/utils/api.js
@@ -1,24 +1,21 @@
+const BASE_URL = "http://localhost:8080/api/lost";
+
 export const fetchItems = async () => {
-  const res = await fetch('/api/items');
+  const res = await fetch(BASE_URL);
   if (!res.ok) throw new Error('Failed to fetch items');
-  const data = await res.json();
-  return data;
+  return await res.json();
 };
 
 export const itemSubmitHandler = async (newItem) => {
   try {
-    const res = await fetch('/api/items', {
+    const res = await fetch(BASE_URL, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(newItem),
     });
 
     if (!res.ok) throw new Error('Failed to post item');
-
-    const savedItem = await res.json();
-
-    return savedItem;
-    // setShowPostModal(false);
+    return await res.json();
   } catch (err) {
     alert(err.message || 'Failed to post item');
   }
