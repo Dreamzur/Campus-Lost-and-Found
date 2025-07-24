d4a448b23180c1870cfa7d9addecaa94a9798651
Dahiver Guerra <roddavid1@gmail.com>
2025-07-23 20:11:03 -0400

    Added findByApprovedTrue() for admin dashboard cards

--- Backend/src/main/java/com/example/demo/repository/LostItemRepository.java
+++ Backend/src/main/java/com/example/demo/repository/LostItemRepository.java
@@ -1,8 +1,9 @@
 package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.demo.model.LostItem;
+import java.util.List;
 
-public interface LostItemRepository extends JpaRepository<LostItem, Integer> {
+public interface LostItemRepository extends JpaRepository<LostItem, Long> {
+  List<LostItem> findByApprovedTrue();
 }
