756e8ac6c77223bd204b83744342cd1564a54e17
Dahiver Guerra <roddavid1@gmail.com>
2025-07-23 20:11:55 -0400

    Implemented Admin priv. and filter tokens

--- Backend/src/main/java/com/example/demo/config/SecurityConfig.java
+++ Backend/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -2,31 +2,51 @@ package com.example.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.example.demo.security.JwtAuthFilter;
+import com.example.demo.security.JwtTokenProvider;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+  private final JwtTokenProvider jwtTokenProvider;
+  private final UserDetailsService userDetailsService;
+
+  public SecurityConfig(JwtTokenProvider jwtTokenProvider, @Lazy UserDetailsService userDetailsService) {
+    this.jwtTokenProvider = jwtTokenProvider;
+    this.userDetailsService = userDetailsService;
+  }
+
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
     http
         .csrf(csrf -> csrf.disable())
         .cors(Customizer.withDefaults())
-
         .authorizeHttpRequests(auth -> auth
-            .requestMatchers("/api/register", "/api/login", "/api/lost").permitAll()
+            .requestMatchers("/api/register", "/api/login", "/api/lost", "/api/found", "/api/image/**").permitAll()
+            .requestMatchers("/api/admin/**").hasRole("ADMIN")
             .anyRequest().authenticated())
-        .httpBasic(Customizer.withDefaults());
+        .addFilterBefore(jwtAuthFilter(), UsernamePasswordAuthenticationFilter.class);
 
     return http.build();
+  }
+
+  @Bean
+  public JwtAuthFilter jwtAuthFilter() {
+    return new JwtAuthFilter(jwtTokenProvider, userDetailsService);
   }
 
   @Bean
