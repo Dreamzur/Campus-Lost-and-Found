81daf47dc1dfcd2f0d361f01d730c01880b98aec
Dahiver Guerra <roddavid1@gmail.com>
2025-07-29 00:56:46 -0400

    Renames & additions to backend

---
+++ Backend/src/main/java/com/example/demo/web/dto/CreateLostItemRequest.java
@@ -0,0 +1,10 @@
+package com.example.demo.web.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record CreateLostItemRequest(
+    String description,
+    @JsonProperty("image_url") String imageUrl,
+    String location,
+    String title) {
+}
